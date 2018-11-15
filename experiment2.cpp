38da19da7489a1c512c9cae6b5017646f4caec4f
divyanshbhasin <41358669+divyanshbhasin@users.noreply.github.com>
2018-11-15 09:44:00 +0530

    Update experiment2.cpp

--- experiment2.cpp
+++ experiment2.cpp
@@ -1,3 +1,7 @@
+/*
+Write a program to accept N numbers from the user and store them in an array. Then, accept
+another number from the user and search that using Linear Search.
+*/
 #include<iostream>
 using namespace std;
 int main()
