316a68c3571530b529fc2ece893e65026d43e025
divyanshbhasin <41358669+divyanshbhasin@users.noreply.github.com>
2018-11-15 09:47:02 +0530

    Update experiment6.cpp

--- experiment6.cpp
+++ experiment6.cpp
@@ -1,3 +1,8 @@
+/*
+Write a program to demonstrate the use of stack in converting arithmetic expression from infix
+notation to postfix notation and in evaluating arithmetic postfix expression.
+*/
+
 #include <iostream>
 #include<math.h>
 #include<stdio.h>
